c5946c754dac8e021ac9db90ff691d1a099f18a5
JunjieZhang <junjie.zhang@live.com>
2017-01-08 19:04:26 +0100

    before homework

---
+++ main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  8 19:03:59 2017
+
+@author: junjie
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  8 18:43:06 2017
+
+@author: junjie
+"""
+
+
+import input_data
+mnist = input_data.read_data_sets("/tmp/data", one_hot = True)
